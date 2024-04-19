bcd36a207351c5d4fa41c17bfac625b20204788d
Shreyas Sawant <shreyassawant018@gmail.com>
2024-04-19 15:59:21 -0400

    NN Visualiser component added

--- autonomous-car/main.js
+++ autonomous-car/main.js
@@ -3,6 +3,7 @@ carCanvas.width = 200;
 const networkCanvas = document.getElementById("networkCanvas");
 networkCanvas.width = 300;
 const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 //Only allotting controls to main car
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
@@ -19,13 +20,14 @@ document.addEventListener("keypress", (e) => {
     car = new Car(road.getLaneCenter(1), 100, 30, 50);
   }
 });*/
-function animate() {
+function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
   car.update(road.borders, traffic);
 
   carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
 
   carCtx.save();
   carCtx.translate(0, -car.y + carCanvas.height * 0.7);
@@ -35,7 +37,9 @@ function animate() {
     traffic[i].draw(carCtx, "blue");
   }
   car.draw(carCtx, "white");
-
+  carCtx.restore();
+  networkCtx.lineDashOffset = -time / 50;
+  NetworkVisualiser.drawNetwork(networkCtx, car.brain);
   carCtx.restore();
   requestAnimationFrame(animate);
 }
--- autonomous-car/utils.js
+++ autonomous-car/utils.js
@@ -38,3 +38,11 @@ function carPolyCrashed(polygon1, polygon2) {
   }
   return false;
 }
+
+function getRGBA(value) {
+  const alpha = Math.abs(value);
+  const R = value < 0 ? 0 : 255;
+  const G = R;
+  const B = value > 0 ? 0 : 255;
+  return "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
+}
